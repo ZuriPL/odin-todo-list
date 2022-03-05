a351a081a46e4d85b041ab963ab1e3ec8db72ff1
zuriii <michal.bak222@gmail.com>
2022-03-05 10:44:08 +0100

    added light/dark theme toggle

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ import './css/body.css'
 const sidebar = sidebarF()
 const topbar = topbarF()
 
+let isLight = true
+function switchTheme() {
+    isLight = !isLight
+    if (isLight) {
+        document.querySelector(':root').style = '--text: black; --bg-1: var(--light-100); --bg-3: var(--light-300);'
+    } else {
+        document.querySelector(':root').style = '--text: white; --bg-1: var(--dark-100); --bg-3: var(--dark-300);'
+    }
+}
 
 document.body.appendChild(sidebar)
 document.body.appendChild(topbar)
+
+document.querySelector('input[type=checkbox]').addEventListener('click', _ => {
+    switchTheme()
+})
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Template',
+      title: 'Todo List',
     }),
   ],
   output: {
